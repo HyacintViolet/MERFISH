3b43d30252591db8ea5dc3b90043f2c9aef7f2a7
luolab <raywong.chn@gmail.com>
2019-01-01 20:02:55 +0800

    SRA_download

--- SRA_download.py
+++ SRA_download.py
@@ -12,18 +12,24 @@ def work(cmd):
 
 def main():
 
+    table_file_dir = '/media/luolab/ZA1BTJWV/SRA_downloads/SraRunTable.txt'
     list_file_dir = '/media/luolab/ZA1BTJWV/SRA_downloads/SRR_Acc_List.txt'
     list = pd.read_csv(list_file_dir, header=None, squeeze=True)
+    table = pd.read_csv(table_file_dir, sep='\t')
+
+    list_mouse = table.loc[table['Organism'] == 'Mus musculus']['Run']
+    list_mouse.reset_index(drop=True)
 
     dst = '/media/luolab/ZA1BTJWV/ncbi/'
+    os.chdir(dst)
 
     cmd_download = []
-    for _, s in list.iteritems():
+    for _, s in list_mouse.iteritems():
         if not os.path.exists(os.path.join(dst, '_'.join([s, '1.fastq.gz']))):
             cmd_this_download = 'fastq-dump --split-files -gzip ' + s
             cmd_download.append(cmd_this_download)
 
-    pool = mp.Pool(16)
+    pool = mp.Pool(1)
     pool.map(work, cmd_download)
 
 
